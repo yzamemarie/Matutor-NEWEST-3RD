6717e17be0d304fea84683545121d21c1a2b1d5b
yzamemarie <ycolinz.yc@gmail.com>
2023-08-22 09:43:53 +0800

    added content youtube embed

--- app/src/main/java/com/example/matutor/Content.java
+++ app/src/main/java/com/example/matutor/Content.java
@@ -2,16 +2,32 @@ package com.example.matutor;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.MediaController;
+import android.widget.SimpleAdapter;
+import android.widget.VideoView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Content extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Content extends AppCompatActivity {
+    SearchView searchBar;
+    RecyclerView recyclerView;
+    WebView videoWebView;
     BottomNavigationView bottomNavigationView;
 
     @Override
@@ -20,9 +36,23 @@ public class Content extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // removes status bar
         setContentView(R.layout.activity_content);
 
-        //Bottom Navigation Code
+        videoWebView = findViewById(R.id.videoWebView);
+        searchBar = findViewById(R.id.searchView);
+        recyclerView = findViewById(R.id.recyclerView);
         bottomNavigationView = findViewById(R.id.bottom_navigator);
         bottomNavigationView.setSelectedItemId(R.id.content);
+
+        //searchbar
+
+        //youtube iframe code from share as embed
+        //width and height replaced as 100% to fit into webview dimensions
+        String link = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/ffLLmV4mZwU\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+        //loads iframe int webview by calling loaddata function and passing mimetype and encoding
+        videoWebView.loadData(link, "text/html", "utf-8");
+        //enable javascript in webview to play video
+        videoWebView.getSettings().setJavaScriptEnabled(true);
+        //set webchromeclient to handle javascript events in app by webview
+        videoWebView.setWebChromeClient(new WebChromeClient());
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
